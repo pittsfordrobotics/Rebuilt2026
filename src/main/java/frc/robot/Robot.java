4834aeb1b32b7fa12138bcc8cb398741c9a23a8a
Larry Moore <lfmoore3@hotmail.com>
2026-01-31 10:55:20 -0500

    Minor cosmetic changes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
+  
   @Logged(name = "RobotContainer")
   private final RobotContainer m_robotContainer;
 
