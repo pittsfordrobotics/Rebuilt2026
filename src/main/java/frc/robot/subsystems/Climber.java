08f7a61bcbf8d8f1288bbb705fa128efaba2bd51
libby du <libbyduhappy@gmail.com>
2026-02-16 14:12:19 -0500

    changed configs

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -6,21 +6,35 @@ package frc.robot.subsystems;
 
 import java.util.function.DoubleSupplier;
 
+import com.ctre.phoenix6.configs.ClosedLoopGeneralConfigs;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static edu.wpi.first.units.Units.*;
 import frc.robot.constants.ClimberConstants;
 
 public class Climber extends SubsystemBase {
     private TalonFX climberMotor = new TalonFX(ClimberConstants.CLIMBER_MOTOR);
     private GenericEntry climberSpeed;
-    
+
     /** Creates a new Climber. */
     public Climber() {
-        TalonFXConfiguration config = new TalonFXConfiguration();
+        TalonFXConfiguration config = new TalonFXConfiguration()
+                .withMotorOutput(
+                        new MotorOutputConfigs()
+                                .withNeutralMode(NeutralModeValue.Brake))
+                .withCurrentLimits(
+                        new CurrentLimitsConfigs()
+                    .withStatorCurrentLimit(Amps.of(120))
+                    .withStatorCurrentLimitEnable(true));
+        
         climberMotor.getConfigurator().apply(config);
 
         climberSpeed = Shuffleboard.getTab("testing").add("Climber Speed", .25).getEntry();
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -6,60 +6,71 @@ package frc.robot.subsystems;
 
 import java.util.function.DoubleSupplier;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.ShooterConstants;
 
+import static edu.wpi.first.units.Units.*;
+
 public class Shooter extends SubsystemBase {
-    /** Creates a new Shooter. */
+	/** Creates a new Shooter. */
 
-    private GenericEntry shooterSpeed;
-    private GenericEntry uptakeSpeed;
+	private GenericEntry shooterSpeed;
+	private GenericEntry uptakeSpeed;
 
-    final TalonFX[] shooterMotors = new TalonFX[ShooterConstants.SHOOTER_MOTORS.length];
-    final TalonFX uptakeMotor = new TalonFX(ShooterConstants.UPTAKE_MOTOR);
+	final TalonFX[] shooterMotors = new TalonFX[ShooterConstants.SHOOTER_MOTORS.length];
+	final TalonFX uptakeMotor = new TalonFX(ShooterConstants.UPTAKE_MOTOR);
 
-    public Shooter() {
-        TalonFXConfiguration config = new TalonFXConfiguration(); //TODO: CONFIGURE 'EM
+	public Shooter() {
+		TalonFXConfiguration config = new TalonFXConfiguration()
+				.withMotorOutput(
+						new MotorOutputConfigs()
+								.withNeutralMode(NeutralModeValue.Coast))
+				.withCurrentLimits(
+						new CurrentLimitsConfigs()
+								.withStatorCurrentLimit(Amps.of(120))
+								.withStatorCurrentLimitEnable(true));
 
-        // instantiate motor controllers
-        for (int i=0; i<shooterMotors.length; i++) {
-            shooterMotors[i] = new TalonFX(ShooterConstants.SHOOTER_MOTORS[i]);
-            shooterMotors[i].getConfigurator().apply(config);
-        }
+		// instantiate motor controllers
+		for (int i = 0; i < shooterMotors.length; i++) {
+			shooterMotors[i] = new TalonFX(ShooterConstants.SHOOTER_MOTORS[i]);
+			shooterMotors[i].getConfigurator().apply(config);
+		}
 
-        uptakeMotor.getConfigurator().apply(config);
+		uptakeMotor.getConfigurator().apply(config);
 
-        shooterSpeed = Shuffleboard.getTab("testing").add("Shooter Motor Speed", .25).getEntry();
-        uptakeSpeed = Shuffleboard.getTab("testing").add("Uptake Motor Speed", .25).getEntry();
-        Shuffleboard.getTab("testing").add("Run Shooter", this.runShooter(() -> shooterSpeed.getDouble(0.25)));
-        Shuffleboard.getTab("testing").add("Run Uptake", this.runUptake(() -> uptakeSpeed.getDouble(0.25)));
-    }
+		shooterSpeed = Shuffleboard.getTab("testing").add("Shooter Motor Speed", .25).getEntry();
+		uptakeSpeed = Shuffleboard.getTab("testing").add("Uptake Motor Speed", .25).getEntry();
+		Shuffleboard.getTab("testing").add("Run Shooter", this.runShooter(() -> shooterSpeed.getDouble(0.25)));
+		Shuffleboard.getTab("testing").add("Run Uptake", this.runUptake(() -> uptakeSpeed.getDouble(0.25)));
+	}
 
+	public Command runShooter(DoubleSupplier speed) {
+		return run(() -> {
+			for (TalonFX motor : shooterMotors) {
+				motor.set(speed.getAsDouble());
+			}
+		}).finallyDo(() -> {
+			for (TalonFX motor : shooterMotors) {
+				motor.set(0);
+			}
+		});
+	}
 
- 
-    public Command runShooter(DoubleSupplier speed) {
-        return run(() -> {
-            for(TalonFX motor: shooterMotors) {
-                motor.set(speed.getAsDouble());
-            }
-        }).finallyDo(() -> {
-            for(TalonFX motor: shooterMotors) {
-                motor.set(0);
-            }
-        });
-    }
+	public Command runUptake(DoubleSupplier speed) {
+		return run(() -> uptakeMotor.set(speed.getAsDouble())).finallyDo(() -> uptakeMotor.set(0));
+	}
 
-    public Command runUptake(DoubleSupplier speed) {
-        return run(() -> uptakeMotor.set(speed.getAsDouble())).finallyDo(() -> uptakeMotor.set(0));
-    }
-   
-    @Override
-    public void periodic() {
-        // This method will be called once per scheduler run
-    }
+	@Override
+	public void periodic() {
+		// This method will be called once per scheduler run
+	}
 }
