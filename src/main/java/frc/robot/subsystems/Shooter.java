65666f58f817cb188bfa5e6aec6f7623cd084350
MEisSCAMMER <ishaan.ramesh@gmail.com>
2026-02-19 15:46:55 -0500

    add hood

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -18,6 +18,7 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -36,6 +37,9 @@ public class Shooter extends SubsystemBase {
 
     @Logged(name="Uptake Motor")
 	final TalonFX uptakeMotor = new TalonFX(ShooterConstants.UPTAKE_MOTOR);
+
+	@Logged(name="Hood Actuator")
+	final Servo hood = new Servo(ShooterConstants.HOOD_ACTUATOR);
 
 	public Shooter() {
 		TalonFXConfiguration shooterConfig = new TalonFXConfiguration()
@@ -107,4 +111,8 @@ public class Shooter extends SubsystemBase {
     public TalonFX getMiddleMotor() {
         return this.shooterMotors[0];
     }
+
+	public Command runHood(DoubleSupplier position) {
+		return run(() -> hood.set(position.getAsDouble()));
+	}
 }
