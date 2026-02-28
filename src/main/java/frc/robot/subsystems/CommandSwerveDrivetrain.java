2a53b0352a6b3ce4c00c8575b4ef8313cd67d887
ivypoolalt <235208037+throwaway3181@users.noreply.github.com>
2026-02-28 14:13:00 -0500

    Update CommandSwerveDrivetrain.java

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -445,6 +445,28 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
         return this.pointAt(() -> AllianceFlipUtil.apply(FieldConstants.blueHubPosition));
     }
 
+    /* IF THE INPUTS ARE SUPPOSED TO BE CARTESIAN PLEASE DONT CONVERT TO POLAR JUST TELL ME PLEASE PLEASE PLEASE <3 <3 <3
+     * xy horizontal, z vertical
+     * direction: 0 = positive x, 90 = positive y
+     * hood angle: 0 = straight up, 90 = horizontal
+     * returns a 3 element array of speed, direction, and hood angle (hood angle is the same)
+     */
+    private double[] calculateOffset(double idealSpeed, double idealDirection, double idealHood, double robotSpeed, double robotDirection) {
+        double robDirRad = Math.toRadians(robotDirection);
+        double vx = robotSpeed * Math.cos(robDirRad);
+        double vy = robotSpeed * Math.sin(robDirRad);
+        double dirRad = Math.toRadians(idealDirection);
+        double hoodRad = Math.toRadians(idealHood);
+        double speedXY = idealSpeed * Math.sin(hoodRad);
+        double[] inCart = {speedXY * Math.cos(dirRad), speedXY * Math.sin(dirRad)};
+        double[] outCart = {inCart[0] - vx, inCart[1] - vy};
+        double rsq = Math.pow(outCart[0], 2)+Math.pow(outCart[1], 2);
+        double r = Math.pow(rsq, 0.5);
+        r /= Math.sin(Math.toRadians(idealHood));
+        double[] out = {r, Math.toDegrees(Math.atan2(outCart[1], outCart[0])), idealHood};
+        return out;
+    }
+
     public Command brake() {
         return this.applyRequest(() -> brake).withInterruptBehavior(InterruptionBehavior.kCancelIncoming);
     }
