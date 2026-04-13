dff18cf1c88323ffa6a180a51f3a92c1d5a89a21
MEisSCAMMER <ishaan.ramesh@gmail.com>
2026-04-13 18:50:02 -0400

    whoops forgot to log vision

--- src/main/java/frc/robot/subsystems/Vision/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision/Vision.java
@@ -30,6 +30,7 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructArrayPublisher;
 import edu.wpi.first.wpilibj.Timer;
 
+@Logged(name="Vision")
 public class Vision extends SubsystemBase {
 
     // Initialization
